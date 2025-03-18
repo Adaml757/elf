cd13972dc75b50223ebdca233303018290cbf422
Jochen Smolka <smolkajochen@gmail.com>
2025-03-18 14:32:32 +0100

    python functions to read/display ELF images

---
+++ tools/export/python/elfpy.py
@@ -0,0 +1,182 @@
+import json
+import h5py
+import os
+import numpy as np
+from numpy import matlib, linalg
+from PIL import Image
+
+def elf_io_readwrite(para:dict, action:str, fname:str=""):
+        """Read/write common ELF data structures. Syntax depends on action, see individual entries below.
+
+        Args:
+            para (dict): Parameter dictionary, obtained from elf_io_readwrite([], "loadpara", fname).
+            action (str): Which load action to perform; see below for valid values.
+            fname (str, optional): Filename; needs to be a full path for action="loadpara", or a scene name for other actions (e.g. "scene001")
+
+        Returns:
+            Return image or dict (depending on action)
+        """
+
+        if action == "loadpara":
+                # load the para structure from the infosum file for this environment
+                # para = elf_io_readwrite([], "loadpara", fname)
+                with open(fname) as f:
+                        data = json.load(f)
+                return data["para"]
+                                
+        if action == "loadinfosum":
+                # load the infosum structure for this environment
+                # para = elf_io_readwrite(para, "loadinfosum")
+                # OR
+                # para = elf_io_readwrite([], "loadinfosum", "scene001")
+                if para:
+                        fname = para["paths"]["fname_infosum_json"]
+                with open(fname) as f:
+                        data = json.load(f)
+                return data["infosum"]
+        
+        elif action == "loadHDR_mat":
+                # load the HDR image for one scene from the mat file (calibrated)
+                # im = elf_io_readwrite(para, "loadHDR_mat", "scene001")
+                (_, f) = os.path.split(fname)
+                fname  = os.path.join(para["paths"]["datapath"], para["paths"]["scenefolder"], f+".mat")
+                with h5py.File(fname, "r") as f:
+                        data = f.get("varinput")
+                        return np.transpose(np.array(data), (2, 1, 0))
+                
+        elif action == "loadHDR_tif":
+                # load the HDR image for one scene from the tif file (uncalibrated)
+                # im = elf_io_readwrite(para, "loadHDR_tif", "scene001")
+                
+                (_, f) = os.path.split(fname)
+                fname  = os.path.join(para["paths"]["datapath"], para["paths"]["scenefolder"], f+".tif")
+                return np.array(Image.open(fname))
+                
+        elif action == "loadfilt_mat":
+                # load the filtered HDR image for one scene from a mat file
+                # im = elf_io_readwrite(para, "loadfilt_mat", "scene001")
+
+                ## TODO: Fix once filter data format has been finalised
+
+                (_, f) = os.path.split(fname)
+                fname  = os.path.join(para["paths"]["datapath"], para["paths"]["filtfolder"], f+".mat")
+                with h5py.File(fname, "r") as f:
+                        data = f.get("varinput")
+                        return np.array(data)
+        
+        elif action == "loadstokes_mat":
+                # load the filtered Stokes vector images for one scene from a mat file
+                # im = elf_io_readwrite(para, "loadstokes_mat", "scene001")
+
+                ## TODO: fix once Stokes data format has been finalised
+                
+                (_, f) = os.path.split(fname)
+                fname  = os.path.join(para["paths"]["datapath"], para["paths"]["filtfolder"], f+"_stokes.mat")
+                with h5py.File(fname, "r") as f:
+                        data = f.get("varinput")
+                        return np.array(data)
+
+        else:
+                raise(Exception(f"Unknown value for parameter action: {action}"))
+        
+  
+def elf_io_correctdng(lin_im:np.ndarray, meta_info:dict=[], method:str="default", maxval:float=0) -> np.ndarray:
+        """ELF_IO_CORRECTDNG takes a linear DNG image (uint16 values), converts its
+           colour space to sRGB and gamma-corrects it for "normal" display (assuming normal white balance is set). 
+           Algorithm adapted from Rob Sumner (2014) "Processing RAW Images in
+           MATLAB", http://users.soe.ucsc.edu/~rcsumner/rawguide/RAWguide.pdf
+
+        Args:
+            lin_im (np.ndarray): linear image to be displayed, can be float, uint8, uint16
+            meta_info (dict, optional): exif info dict, only needed for the ColorMatrix2 value, so can be infosum. Defaults to the standard D800 colour matrix.
+            method (str, optional): determines what value the image is normalised to:    
+                                "bitdepth" : default method, normalise depending on maximum of lin_im 
+                                             (1 if max<1, 2^8 if max<2^8, 2^16 if max<2^16, max otherwise)
+                                "max"      : normalise to the maximum of lin_im
+                                "maxval"   : normalise to the input argument maxval
+                                "bright"/"maxbright"/"maxvalbright" : use a bright version of gamma correction
+            maxval (float, optional): Normalisation value for the "maxval" method
+
+        Returns:
+            np.ndarray: Corrected image
+        """
+
+        def sub_apply_cmatrix(im, cmatrix):
+                # CORRECTED = sub_apply_cmatrix(IM, CMATRIX)
+                #
+                # Applies CMATRIX to RGB input IM. Finds the appropriate weighting of the
+                # old color planes to form the new color planes, equivalent to but much
+                # more efficient than applying a matrix transformation to each pixel.
+
+                if im.shape[2]!=3:
+                        raise(ValueError("Apply cmatrix to RGB image only."))
+
+                r = cmatrix[0,0]*im[:,:,0:1] + cmatrix[0,1]*im[:,:,1:2] + cmatrix[0,2]*im[:,:,2:3]
+                g = cmatrix[1,0]*im[:,:,0:1] + cmatrix[1,1]*im[:,:,1:2] + cmatrix[1,2]*im[:,:,2:3]
+                b = cmatrix[2,0]*im[:,:,0:1] + cmatrix[2,1]*im[:,:,1:2] + cmatrix[2,2]*im[:,:,2:3]
+                return np.concatenate((r, g, b), 2)
+
+
+        def sub_srgbGamma(im):
+                # im = sub_srgbGamma(im)
+                #
+                # Applies  a standard (inverse) sRGB gamma correction to an RGB image.
+                # Assumes input values are scaled between 0 and 1, returns a similar range.
+                
+                nl          = im > 0.0031308  # anything > a count of ~205
+                im[nl]      = 1.055 * im[nl]**(1/2.4) - 0.055
+                im[~nl]     = 12.92 * im[~nl]
+                return im
+
+        def sub_rgb2gray(im):
+                # Turns an RGB image into a grayscale image using matlab default values
+                return 0.298936021293775 * im[:, :, 0] + 0.587043074451121 * im[:, :, 1] + 0.114020904255103 * im[:, :, 2] 
+        
+
+        if (not meta_info) or (len(meta_info["ColorMatrix2"]) == 1 and meta_info["ColorMatrix2"] == 0):
+                meta_info["ColorMatrix2"] = np.array([0.7866, -0.2108, -0.0555, -0.4869, 1.2483, 0.2681, -0.1176, 0.2069, 0.7501])
+                print("Warning: io_correctdng: No valid colour correction matrix provided. Using standard D800 matrix.")
+
+        ## Parameters
+        # create conversion matrices
+        rgb2xyz  = np.array([[.4124564, .3575761, .1804375], [.2126729, .7151522, .0721750], [.0193339, .1191920, .9503041]])  # from Adobe sRGB to XYZ colour space
+        xyz2cam  = np.reshape(meta_info["ColorMatrix2"], (3, 3)) # from XYZ to camera colour space
+        rgb2cam  = xyz2cam @ rgb2xyz                      # from sRGB to camera colour space
+        rgb2cam  = rgb2cam / np.transpose(matlib.repmat(np.sum(rgb2cam,1), 3, 1))  # normalize rows to 1
+        cam2rgb  = linalg.inv(rgb2cam)                             # from camera to sRGB colour space
+
+        ## 1) Normalise
+        lin_im   = lin_im.astype("float")
+        if method in ["default", "bitdepth", "bright"]:
+                if np.max(lin_im) <= 1:
+                        mv = 1
+                elif np.max(lin_im) <= 2^8:
+                        mv = 2^8
+                elif np.max(lin_im) <= 2^16:
+                        mv = 2^16 
+                else:
+                        mv = np.max(lin_im)        
+        elif method in ["max", "maxbright"]:
+                mv   = np.max(lin_im)
+        elif method in ["maxval", "maxvalbright"]:
+                mv = maxval
+        else:
+                raise(ValueError(f"Unknown correctdng method: {method}"))
+        lin_im   = lin_im / mv
+
+        ## 2) Colour Space Conversion to sRGB
+        lin_srgb = sub_apply_cmatrix(lin_im, cam2rgb);     # apply conversion matrix
+        lin_srgb = np.clip(lin_srgb, 0, 1);                # Always keep image clipped b/w 0-1
+
+        ## 3) Gamma correction
+        im       = sub_srgbGamma(lin_srgb)
+        if method=="bright" or method=="maxvalbright" or method=="maxbright":
+                ## 3) Gamma correction (bright version)
+                grayim      = sub_rgb2gray(lin_srgb)
+                grayscale   = 0.25/np.mean(grayim)
+                bright_srgb = np.clip(lin_srgb*grayscale, None, 1)
+                im          = sub_srgbGamma(bright_srgb)
+
+        return im
+
+        
---
+++ tools/export/python/jupyter_helpers.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+from typing import Optional, Union
+import numpy as np
+
+def display_img(img: Union[np.ndarray, list], cmap: str = 'gray', figsize: tuple = (10,2), axtitle: Union[str, list] = "", figtitle: str = '') -> None:
+    """Display one or more images in a large-enough window in a jupyter notebook
+
+    Args:
+        img (np.ndarray): The image to display
+        cmap (str, optional): Colormap to use for GS images. Defaults to 'gray'.
+        figsize (tuple, optional): Figure size for whole grid in inches. Defaults to (10,10).
+
+    Args:
+        img (np.ndarray | list): The image or list of images to display
+        cmap (str, optional): Colormap to use for GS images. Defaults to 'gray'.
+        figsize (tuple, optional): Figure size (w, h) for whole grid in inches. Defaults to (10,2).
+        axtitle (str | list, optional): _description_. Defaults to "".
+        figtitle (str, optional): _description_. Defaults to ''.
+
+    Raises:
+        ValueError: _description_
+    """
+    if isinstance(img, list):
+        imglist = img
+    else:
+        imglist = [img,]
+
+    if isinstance(axtitle, list):
+        axtitlelist = axtitle
+    else:
+        axtitlelist = len(imglist)*[axtitle,]
+
+    if len(axtitlelist) != len(imglist):
+        raise ValueError("The list of images and that of plot titles must be the same length")
+
+    fig = plt.figure(figsize=figsize)
+    for i, I in enumerate(imglist):
+        ax = fig.add_subplot(1, len(imglist), i+1)
+        ax.imshow(I, cmap=cmap)
+        if axtitlelist[i]:
+            plt.title(axtitlelist[i])
+            # ax.set_title(axtitlelist[i])
+    if figtitle:
+        fig.suptitle(figtitle)
+    fig.show()
+
+
+def display_img_stack(stack: np.ndarray, cmap: str='gray', ncols: Optional[int] = None, figsize: tuple = (24,24), figtitle: str=''):
+    """Display an image stack as subplots in a large-enough window in a jupyter notebook
+
+    Args:
+        stack (np.ndarray): Image stack as a 3d array (z,y,x) or 4d array (z,y,x,c).
+        cmap (str, optional): Colormap to use for GS images. Defaults to 'gray'.
+        ncols (int, optional): Number of columns to organise images into. Defaults to None: make a square grid.
+        figsize (tuple, optional): Figure size (w, h) for whole grid in inches. Defaults to (24,24).
+    """
+    if ncols is None:
+        ncols = int(np.ceil(np.sqrt(stack.shape[0])))
+    nrows = int(np.ceil(stack.shape[0]/ncols))
+    fig = plt.figure(figsize=(figsize[0], figsize[1]/ncols*nrows))
+    if figtitle:
+        fig.suptitle(figtitle)
+    for i in range(0, stack.shape[0]):
+        ax = fig.add_subplot(nrows, ncols, i+1)
+        ax.imshow(stack[i, :, :], cmap=cmap)
+        plt.title(f"{i}")
+    plt.show()
