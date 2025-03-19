8160017f2145214c47789217e90c654dff0c02d0
Jochen Smolka <smolkajochen@gmail.com>
2025-03-19 11:56:10 +0100

    new CORE module parameters

--- modules/core/core_analysisPara.m
+++ modules/core/core_analysisPara.m
@@ -1,22 +1,32 @@
 function anaP = core_analysisPara(d)
+    % translate .env parameters for the CORE module
 
-anaParameters = ...
-    {'RESOLUTION_BOOSTER', 'double';
-     'CALCULATE_INT', 'logical';
-     'CALCULATE_MEAN_IMAGE', 'logical';
-     'SAVE_SCENE_TIFS', 'logical';
-     'TARGET_PROJECTION', 'string';
-     'TARGET_AZI_RANGE', 'doublevector';
-     'TARGET_ELE_RANGE', 'doublevector';
-     'HDIVN_INT', 'double';
-     'RANGE_PERC', 'double';
-     'COLOUR_CALIB_TYPE', 'string';
-     'INT_ANALYSIS_TYPE', 'string';
-     'HDR_METHOD', 'string';};
+    anaParameters = ...
+        {'RESOLUTION_BOOSTER', 'double';
+        'CALCULATE_INT', 'logical';
+        'CALCULATE_MEAN_IMAGE', 'logical';
+        'IMAGE_DIRECTION', 'string'; %%
+        'TARGET_PROJECTION', 'string';
+        'TARGET_AZI_RANGE', 'doublevector';
+        'TARGET_ELE_RANGE', 'doublevector';
+        'HDIVN_INT', 'double';
+        'RANGE_PERC', 'double';
+        'COLOUR_CALIB_TYPE', 'string';
+        'INT_ANALYSIS_TYPE', 'string';
+        'HDR_METHOD', 'string';
+        'SAVE_SCENE_TIFS', 'logical';
+        'SAVE_DIAGNOSTIC_TIFS', 'logical'}; %%
 
-anaP = d.extractValues('ANALYSIS', anaParameters);
-anaP.version = str2double(d.Env.VERSION);
+    anaP = d.extractValues('ANALYSIS', anaParameters);
+    anaP.version = str2double(d.Env.VERSION);
 
-if isempty(anaP.resolutionBooster) || anaP.resolutionBooster ~= round(anaP.resolutionBooster) || anaP.resolutionBooster<=0 || anaP.resolutionBooster>10
-    error('Invalid value found for ANALYSIS_RESOLUTION_BOOSTER in %s', fullfile(envPath, '.env'));
+    if isempty(anaP.resolutionBooster) || anaP.resolutionBooster ~= round(anaP.resolutionBooster) || anaP.resolutionBooster<=0 || anaP.resolutionBooster>10
+        error('Invalid value found for ANALYSIS_RESOLUTION_BOOSTER');
+    end
+    if numel(anaP.targetAziRange)~=2
+        error("ANALYSIS_TARGET_AZI_RANGE needs to have exactly two elements");
+    end
+    if numel(anaP.targetEleRange)~=2
+        error("ANALYSIS_TARGET_ELE_RANGE needs to have exactly two elements");
+    end
 end
